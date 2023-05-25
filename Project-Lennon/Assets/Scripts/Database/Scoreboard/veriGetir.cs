ac78a526af89f2e7a7c2d58b986dbfd8b531565c
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-05-25 14:05:25 +0300

    Scoreboard PHP connection script and Scoreboard UI

---
+++ Project-Lennon/Assets/Scripts/Database/Scoreboard/veriGetir.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class veriGetir : MonoBehaviour
+{
+    public string[] gelen_veriler = new string[10];//Formdan gelen veriler için array
+    public TMP_Text[] textler;
+    void Start()
+    {
+        StartCoroutine(kayit_cek());
+    }
+    
+    IEnumerator kayit_cek()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("komut", "veriCekme");
+        string url = "http://localhost/scoreboard.php";
+        WWW w = new WWW(url, form);
+        yield return w;
+        gelen_veriler = w.text.Split(';');
+
+        for(int i =0; i < gelen_veriler.Length; i++)
+        {
+            textler[i].text = "" + gelen_veriler[i];   
+
+        }
+        Debug.Log(w.text);
+    }
+}
